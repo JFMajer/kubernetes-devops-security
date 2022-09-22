cd7fb6b05c6b76ea0ad6c894d3baa6376b3afac0
JFMajer <jakubmajer@live.com>
2022-09-22 17:04:37 +0200

    added k8s deployment opa check

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -3,4 +3,4 @@
 sed -i "s|{{image}}|${imageName}|g" k8s_deployment_service.yaml
 kubectl -n default get deployment ${deploymentName} > /dev/null
 
-kubectl -n default apply -f k8s_deployment_service.yaml
+kubectl -n default apply -f k8s_deployment_service.yaml --record
