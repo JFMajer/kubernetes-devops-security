dfd2c1d8e40269aff9fdc9264a40ab820eb8bad3
JFMajer <jakubmajer@live.com>
2022-09-22 13:21:42 +0200

    added trivy scan

--- trivy-scan.sh
+++ trivy-scan.sh
@@ -3,8 +3,8 @@
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
-docker run --rm -v docker run --rm -v aquasec/trivy -q image --exit-code 0 --severity HIGH --light $dockerImageName
-docker run --rm -v docker run --rm -v aquasec/trivy -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+docker run --rm -v docker run --rm -v aquasec/trivy:0.32.0 -q image --exit-code 0 --severity HIGH --light $dockerImageName
+docker run --rm -v docker run --rm -v aquasec/trivy:0.32.0 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
 exit_code=$?
 echo "Exit code: $exit_code"
